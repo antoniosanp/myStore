048c9e56b549812aa388da6a10af56d1334cb302
antoniosanp <antonio.pulgarin97@gmail.com>
2026-09-03 14:57:09 -0500

    feat(repository): order & orderItem repo

---
+++ backend/src/main/java/com/myStore/backend/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package com.myStore.backend.repository;
+
+import com.myStore.backend.model.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    @Override
+    @EntityGraph(attributePaths = {"items", "items.product", "user"})
+    Optional<Order> findById(UUID id);
+
+    @EntityGraph(attributePaths = {"items", "items.product", "user"})
+    Optional<Order> findByIdAndUserId(UUID id, UUID userId);
+
+    @EntityGraph(attributePaths = {"items", "items.product"})
+    List<Order> findByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    @Override
+    @EntityGraph(attributePaths = {"items", "items.product", "user"})
+    List<Order> findAll();
+}
