638a00c1406e03a73bdaae9300460197a9a375e5
antoniosanp <antonio.pulgarin97@gmail.com>
2026-09-04 08:30:14 -0500

    feat(config): rabbitmq config

---
+++ backend/src/main/java/com/myStore/backend/messaging/config/RabbitMQConfig.java
@@ -0,0 +1,75 @@
+package com.myStore.backend.messaging.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${app.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${app.rabbitmq.order-created-queue}")
+    private String orderCreatedQueue;
+
+    @Value("${app.rabbitmq.order-created-routing-key}")
+    private String orderCreatedRoutingKey;
+
+    @Value("${app.rabbitmq.payment-completed-queue}")
+    private String paymentCompletedQueue;
+
+    @Value("${app.rabbitmq.payment-completed-routing-key}")
+    private String paymentCompletedRoutingKey;
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Queue orderCreatedQueue() {
+        return QueueBuilder.durable(orderCreatedQueue).build();
+    }
+
+    @Bean
+    public Queue paymentCompletedQueue() {
+        return QueueBuilder.durable(paymentCompletedQueue).build();
+    }
+
+    @Bean
+    public Binding orderCreatedBinding(Queue orderCreatedQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(orderCreatedQueue)
+                .to(exchange)
+                .with(orderCreatedRoutingKey);
+    }
+
+    @Bean
+    public Binding paymentCompletedBinding(Queue paymentCompletedQueue, DirectExchange exchange) {
+        return BindingBuilder.bind(paymentCompletedQueue)
+                .to(exchange)
+                .with(paymentCompletedRoutingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+}
