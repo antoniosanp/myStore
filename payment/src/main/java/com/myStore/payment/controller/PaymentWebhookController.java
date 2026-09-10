43d2bc2bad47678c396208ecd3948bcf6e869278
antoniosanp <antonio.pulgarin97@gmail.com>
2026-09-10 13:57:54 -0500

    feat(CORS): added CORS so Mercado Pago can access the webhook

--- payment/src/main/java/com/myStore/payment/controller/PaymentWebhookController.java
+++ payment/src/main/java/com/myStore/payment/controller/PaymentWebhookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
